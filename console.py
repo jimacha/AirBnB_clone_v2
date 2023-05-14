78c96e427dd2d4ff51953391bebfa8f4b6b93767
Programlucy <117766910+Programlucy@users.noreply.github.com>
2023-05-14 18:16:28 +0300

    Update console.py

--- console.py
+++ console.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
-from models import Storage
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+"""Defines the HBnB console."""
 import cmd
 
 """
